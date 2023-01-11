8da15285d5a71634c920af4ca8eec719cb559b3b
lucaasgarcia <lukas.garciaxd@icloud.com>
2023-01-10 23:52:02 -0300

    atualizacao do readme

---
+++ README.md
@@ -0,0 +1,18 @@
+# Formulario simples de contato
+
+Form feito para fins de estudos e para o portifolio.
+
+### Preview
+![preview](/public/images/preview.png "Imagem de Preview")
+
+
+### Passos concluidos
+
+- [x] Criacao do HTML do formulario
+- [x] Adicionada funcao de envio de email
+
+### Clone este repositorio
+
+```bash
+git clone https://github.com/lucaasgarcia/simpleFormContact.git
+```
--- enviar.php
+++ enviar.php
@@ -1,1 +1,18 @@
 <?php
+    $to = "contato@contato.com.br"; //destinatario
+    $assunto = "Simple form PHP";
+    $mensagem = $_POST['mensagem']." <br><br> ".$_POST['nome'];
+    $email = $_POST['email']; //remetente
+
+    $headers[] = 'MIME-Version: 1.0';
+    $headers[] = 'Content-type: text/html; charset=iso-8859-1';
+    $headers[] = 'To: $to';
+    $headers[] = 'From: $email';
+
+    $status = mail($to, $subject, $message, implode("\r\n", $headers));
+
+    if($status==true){
+        print "mensagem enviada com sucesso!";
+    }else{
+        print "Nao foi possivel enviar";
+    }
